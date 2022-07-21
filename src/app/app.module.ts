9b3ff83d1d70f5949bce978358047e9b7abb9ddb
Ronald Terrones Celis <ronald.tc14@gmail.com>
2022-07-21 17:30:40 -0500

    Primera version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainComponent } from './components/main/main.component';
+import { SiderbarComponent } from './components/siderbar/siderbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    MainComponent,
+    SiderbarComponent
   ],
   imports: [
     BrowserModule
